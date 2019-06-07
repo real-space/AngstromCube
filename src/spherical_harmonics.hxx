e16021cd9da786770d56d9b26702449e2f450f0b
Paul Baumeister <p.baumeister@fz-juelich.de>
2019-06-07 16:59:22 +0200

    simplify in spherical_harmonics

--- src/spherical_harmonics.hxx
+++ src/spherical_harmonics.hxx
@@ -6,8 +6,8 @@
 namespace spherical_harmonics {
 
   template<typename real_t, typename vector_real_t>
-  int Ylm(std::complex<real_t> ylm[], int const ellmax, vector_real_t const v[3]) {
-    
+  void Ylm(std::complex<real_t> ylm[], int const ellmax, vector_real_t const v[3]) {
+    if (ellmax < 0) return;
 // !************************************************************
 // !     generate the spherical harmonics for the vector v
 // !     using a stable upward recursion in l.  (see notes
@@ -84,15 +84,15 @@ namespace spherical_harmonics {
          sph = 0;
       }
 
-      int const S = (1 + ellmax); // stride
+      int const S = (1 + ellmax); // stride for p, the array of associated legendre functions
       real_t p[(1 + ellmax)*S];
 
-// !---> generate associated legendre functions for m.ge.0
+// !---> generate associated legendre functions for m >= 0
       real_t fac = 1;
 // !---> loop over m values
       for(int m = 0; m < ellmax; ++m) {
          fac *= (1 - 2*m);
-         p[m + S*m] = fac;
+         p[m     + S*m] = fac;
          p[m + 1 + S*m] = (m + 1 + m)*cth*fac;
 // !--->    recurse upward in l
          for(int l = m + 2; l <= ellmax; ++l) {
@@ -102,37 +102,29 @@ namespace spherical_harmonics {
       } // m
       p[ellmax + S*ellmax] = (1 - 2*ellmax)*fac;
 
-//       real_t c[1 + ellmax], s[1 + ellmax];
-// // !--->    determine sin and cos of phi
-//       s[0] = 0; s[1] = sph;
-//       c[0] = 1; c[1] = cph;
-//       auto const cph2 = 2*cph;
-//       for(int m = 2; m <= ellmax; ++m) {
-//           s[m] = cph2*s[m - 1] - s[m - 2];
-//           c[m] = cph2*c[m - 1] - c[m - 2];
-//       } // m
-      std::complex<real_t> cpis[1 + ellmax];
-      cpis[0] = std::complex<real_t>(1, 0);
-      cpis[1] = std::complex<real_t>(cph, sph);
-      for(int m = 2; m <= ellmax; ++m) {
-          cpis[m] = cpis[m - 1]*cpis[1];
-      } // m
+      real_t c[1 + ellmax], s[1 + ellmax];
+// !--->    determine sin and cos of phi
+      c[0] = 1; s[0] = 0;
+      if (ellmax > 0) {
+          c[1] = cph; s[1] = sph; 
+          auto const cph2 = 2*cph;
+          for(int m = 2; m <= ellmax; ++m) {
+              s[m] = cph2*s[m - 1] - s[m - 2];
+              c[m] = cph2*c[m - 1] - c[m - 2];
+          } // m
+      } // ellmax > 0
 
 // !--->    multiply in the normalization factors
-      for(int l = 0; l <= ellmax; ++l) {
-          int const lm0 = l*l + l;
-          ylm[lm0] = ynorm[lm0]*p[l + S*0]*cpis[0]; // std::complex<real_t>(1, 0);
-      } // l
-      for(int m = 1; m <= ellmax; ++m) {
+      for(int m = 0; m <= ellmax; ++m) {
           for(int l = m; l <= ellmax; ++l) {
               int const lm0 = l*l + l;
-              auto const ylms = p[l + S*m]*cpis[m]; // std::complex<real_t>(c[m], s[m]);
+              auto const ylms = p[l + S*m]*std::complex<real_t>(c[m], s[m]);
               ylm[lm0 + m] = ynorm[lm0 + m]*ylms;
               ylm[lm0 - m] = ynorm[lm0 - m]*std::conj(ylms);
           } // l
       } // m
 
-      return 0;
+      return;
   } // Ylm
 
 //   int all_tests();
