8b60c5f8a7d82abb89461ba8ae876e02c32cf44e
Paul F Baumeister <p.baumeister@fz-juelich.de>
2019-08-01 03:47:32 +0200

    simplify header

--- src/recorded_warnings.hxx
+++ src/recorded_warnings.hxx
@@ -1,16 +1,12 @@
 #pragma once
 
-#include <cstdint> // uint64_t
-#include <string> // std::string
-#include <cstdio> // printf
-
 typedef int status_t;
 
 #define warn recorded_warnings::new_warning(__FILE__, __LINE__) 
 
 namespace recorded_warnings {
 
-  char* new_warning(char const *file, int const line);
+  char* new_warning(char const *file, int const line); // please use the macro above
 
   status_t show_warnings(int const echo=1);
 
