9d64c3919a8a65f3d0333dafc281dd55335ae92f
Paul Baumeister <p.baumeister@fz-juelich.de>
2020-01-17 17:41:20 +0100

    davidson still does not solve the particle-in-a-box Hamiltonian correctly

--- src/davidson_solver.cxx
+++ src/davidson_solver.cxx
@@ -2,6 +2,7 @@
 #include <cassert> // assert
 #include <vector> // std::vector<T>
 #include <algorithm> // std::swap<T>
+#include <cmath> // std::sin
 
 #include "davidson_solver.hxx"
 
@@ -36,7 +37,7 @@ namespace davidson_solver {
   // solve iteratively for the lowest eigenstates of an implicitly given Hamiltonian using the Davidson method
   
   template<typename real_t, int D0> // D0: vectorization
-  void inner_products(real_t s[] // result <bra|ket> [nstates]
+  void inner_products(real_t s[] // result <bra|ket> [nstates][nstates]
                    , int const stride // same stride assumed for both, bra and ket
                    , size_t const ndof
                    , real_t const bra[] // assumed shape [nstates/D0][ndof][D0]
@@ -44,6 +45,7 @@ namespace davidson_solver {
                    , real_t const ket[] // assumed shape [nstates/D0][ndof][D0]
                    , int const mstates  // number of ket states
                    , real_t const factor=1) {
+      assert(stride >= mstates);
       for(int istate = 0; istate < nstates; ++istate) {     int const ivec = istate/D0, i0 = istate % D0;
           for(int jstate = 0; jstate < mstates; ++jstate) { int const jvec = jstate/D0, j0 = jstate % D0;
               real_t tmp = 0;
@@ -56,6 +58,23 @@ namespace davidson_solver {
       } // istate
   } // inner_products
 
+  
+  template<typename real_t, int D0> // D0: vectorization
+  void vector_norm2s(real_t s[] // result <ket|ket> [mstates]
+                   , size_t const ndof
+                   , real_t const ket[] // assumed shape [nstates/D0][ndof][D0]
+                   , int const mstates  // number of ket states
+                   , real_t const factor=1) {
+      for(int jstate = 0; jstate < mstates; ++jstate) {     int const jvec = jstate/D0, j0 = jstate % D0;
+          real_t tmp = 0;
+          for(size_t dof = 0; dof < ndof; ++dof) {
+              tmp += ket[(jvec*ndof + dof)*D0 + j0]
+                   * ket[(jvec*ndof + dof)*D0 + j0];
+          } // dof
+          s[jstate] = tmp*factor; // init
+      } // jstate
+  } // vector_norm2s
+  
   
   template<typename real_t>
   void show_matrix(real_t const mat, int const stride, int const n, int const m, char const *name=nullptr) {
@@ -78,6 +97,8 @@ namespace davidson_solver {
       size_t const ndof = (size_t)g.dim(0) * (size_t)g.dim(1) * (size_t)g.dim(2);
       int const mbasis = 2; // control::get("davidson.basis.multiplier", 2);
       if (echo > 0) printf("# start Davidson (subspace size up to %i x %i bands)\n", mbasis, nbands);
+
+      double const threshold2 = 1e-8;
       
       int const max_space = mbasis*nbands;
       int       sub_space = nbands; // init with the waves from the input
@@ -102,6 +123,8 @@ namespace davidson_solver {
       std::vector<real_t> spsi(max_space*g.all(), 0.0);
       std::vector<real_t> epsi(max_space*g.all(), 0.0); // new eigenfunctions
       std::vector<real_t> eigval(max_space);
+      std::vector<real_t> residual_norm2s(nbands);
+      std::vector<int> band_index(nbands);
       
       for(int iteration = 0; iteration < 2; ++iteration) {
           if (echo > 0) printf("# Davidson iteration %i\n", iteration);
@@ -153,7 +176,31 @@ namespace davidson_solver {
                   } // k
               } // i
 
-              sub_space += nbands; // increase the subspace size by nbands residual vectors
+              int new_bands{0};
+              vector_norm2s<real_t,D0>(residual_norm2s.data(), ndof, epsi.data() + nbands*g.all(), nbands);
+              {
+                  for(int ires = 0; ires < nbands; ++ires) { // serial, loop-carried dependency in new_bands
+                      if (residual_norm2s[ires] > threshold2) {
+                          band_index[new_bands] = ires;
+                          ++new_bands;
+                      }
+                  } // ires
+              }
+              
+              // re-pack the new residuals
+              for(int ires = 0; ires < new_bands; ++ires) { // serial due to RAW dependencies
+                  int const itarget = nbands + ires;
+                  int const isource = nbands + band_index[ires];
+                  if (itarget < isource) {
+                      for(int k = 0; k < stride; ++k) {
+                          epsi[itarget*stride + k] = epsi[isource*stride + k];
+                      } // k
+                  } else stat += (itarget > isource); // errors
+              } // ires
+
+              sub_space += new_bands; // increase the subspace size by the number of non-vanishing residual vectors
+              
+              if (new_bands < nbands) { printf("# add %i residuals\n", new_bands); exit(__LINE__); }
 
           } // not in the second iteration
 
@@ -180,29 +227,36 @@ namespace davidson_solver {
       int const dims[] = {8, 8, 8};
       // particle in a box: lowest mode: sin(xyz*pi/L)^3 --> k_x=k_y=k_z=pi/L
       // --> ground state energy = 3*(pi/9)**2 Rydberg = 0.182 Hartree
-      //                           3*(pi/8.78)^2       = 0.192 (found)
+      //                           3*(pi/8.78)**2      = 0.192 (found)
       //                           3*(pi/8)**2         = 0.231
-      // first excitation energies should be 2*(pi/9)^2 + (3*pi/9)^2 = 0.67 Hartree (3-fold degenerate)
+      // first excitation energies should be 2*(pi/9)**2 + (2*pi/9)**2 = 0.384 Hartree (3-fold degenerate)
       real_space_grid::grid_t<D0> g(dims);
       std::vector<double> psi(nbands*g.all(), 0.0);
-      { // scope: create good start wave functions
+      
+      if (0) { // scope: create good start wave functions
           double const k = constants::pi/8.78; // ground state wave vector
           double wxyz[8] = {1, 0,0,0, 0,0,0, 0};
-          for(int iz = 0; iz < dims[2]; ++iz) { wxyz[3] = iz - 3.5; double const sin_z = sin(k*wxyz[3]); 
-          for(int iy = 0; iy < dims[1]; ++iy) { wxyz[2] = iy - 3.5; double const sin_y = sin(k*wxyz[2]);
-          for(int ix = 0; ix < dims[0]; ++ix) { wxyz[1] = ix - 3.5; double const sin_x = sin(k*wxyz[1]);
-              if (nbands > 4) { 
-                  wxyz[4] = wxyz[1]*wxyz[2]; // x*y 
-                  wxyz[5] = wxyz[2]*wxyz[3]; // y*z
-                  wxyz[6] = wxyz[3]*wxyz[1]; // z*x
+          for(int iz = 0; iz < dims[2]; ++iz) { wxyz[3] = iz - 3.5; double const sin_z = std::sin(k*wxyz[3]); 
+          for(int iy = 0; iy < dims[1]; ++iy) { wxyz[2] = iy - 3.5; double const sin_y = std::sin(k*wxyz[2]);
+          for(int ix = 0; ix < dims[0]; ++ix) { wxyz[1] = ix - 3.5; double const sin_x = std::sin(k*wxyz[1]);
+              if (nbands > 4) {
+                  wxyz[4] = wxyz[1]*wxyz[2]; // x*y (ell=2)
+                  wxyz[5] = wxyz[2]*wxyz[3]; // y*z (ell=2)
+                  wxyz[6] = wxyz[3]*wxyz[1]; // z*x (ell=2)
                   wxyz[7] = wxyz[1]*wxyz[2]*wxyz[3]; // x*y*z (ell=3)
-              }
+              } // nbands > 4
               int const ixyz = (iz*dims[1] + iy)*dims[0] + ix;
               for(int iband = 0; iband < nbands; ++iband) {
                   psi[iband*g.all() + ixyz] = wxyz[iband]*sin_x*sin_y*sin_z; // good start wave functions
               } // iband
           }}} // ix iy iz
       } // scope: create good start wave functions
+      else {
+          for(int iband = 0; iband < nbands; ++iband) {
+              psi[iband*g.all() + iband] = 1; // bad start wave functions
+          } // iband
+      }
+      
       for(int it = 0; it < 9999; ++it) {
           stat += solve(psi.data(), nbands, g, echo);
       } // it
