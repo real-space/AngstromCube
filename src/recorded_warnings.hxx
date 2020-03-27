ec2aebe0b6d3011ae39754b923e7a7d51f851fa3
Paul F Baumeister <p.baumeister@fz-juelich.de>
2020-03-27 12:05:35 +0100

    restructure the warn(...) macro

--- src/recorded_warnings.hxx
+++ src/recorded_warnings.hxx
@@ -2,39 +2,36 @@
 
 #include "status.hxx" // status_t
 
-#include <cstdio> // fprintf, std::sprintf, stdout, stderr
+#include <cstdio> // std::fprintf, std::sprintf, stdout, stderr, std::fflush
 #include <utility> // std::forward
 
-// #define warn(...) std::sprintf(recorded_warnings::_new_warning(__FILE__, __LINE__, __func__), __VA_ARGS__); 
 
-#define warn(...) { \
-    
-    std::sprintf(recorded_warnings::_new_warning(__FILE__, __LINE__, __func__), __VA_ARGS__); \
-    }
+
+namespace recorded_warnings {
+
+// #define warn(...) std::sprintf(recorded_warnings::_new_warning(__FILE__, __LINE__, __func__), __VA_ARGS__);
+  char* _new_warning(char const *file, int const line, char const *func); // hidden, please use the macro above
 
 #define error(...) { \
     recorded_warnings::_print_error_message(stdout, __FILE__, __LINE__, __VA_ARGS__ ); \
     recorded_warnings::_print_error_message(stderr, __FILE__, __LINE__, __VA_ARGS__ ); \
     exit(__LINE__); }
-
-namespace recorded_warnings {
-
-  char* _new_warning(char const *file, int const line, char const *func); // hidden, please use the macro above
-
+    
   template <class... Args>
   void _print_error_message(FILE* os, char const *srcfile, int const srcline, Args &&... args) {
-        fprintf(os, "\n\n# Error in %s:%i  Message:\n#   ", srcfile, srcline);
-        fprintf(os, std::forward<Args>(args)...);
-        fprintf(os, "\n\n");
-        fflush(os);
+        std::fprintf(os, "\n\n# Error in %s:%i  Message:\n#   ", srcfile, srcline);
+        std::fprintf(os, std::forward<Args>(args)...);
+        std::fprintf(os, "\n\n");
+        std::fflush(os);
   } // _print_error_message
-
+  
+#define warn(...) recorded_warnings::_print_warning_message(__FILE__, __LINE__, __func__, __VA_ARGS__);
   template <class... Args>
-  void _print_warning_message(FILE* os, char const *srcfile, int const srcline, Args &&... args) {
-        fprintf(os, "\n\n# Error in %s:%i  Message:\n#   ", srcfile, srcline);
-        fprintf(os, std::forward<Args>(args)...);
-        fprintf(os, "\n\n");
-        fflush(os);
+  int _print_warning_message(char const *srcfile, int const srcline, char const* func, Args &&... args) {
+      auto const str = _new_warning(__FILE__, __LINE__, __func__);
+      // generate the warning message
+      int const nchars = std::sprintf(str, std::forward<Args>(args)...);
+      return nchars;
   } // _print_warning_message
   
   status_t show_warnings(int const echo=1);
