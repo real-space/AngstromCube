6ba66d22084a9d71f164ea66317029fb7120ecdb
Paul F Baumeister <p.baumeister@fz-juelich.de>
2020-04-08 16:06:18 +0200

    multi-grid 1D shows deteriorating convergence with increasing system size

--- src/multi_grid.test.sh
+++ src/multi_grid.test.sh
@@ -1,31 +1,35 @@
 #!/usr/bin/env bash
 
 echo -n "# " ## comment out the output of make, e.g. "make: Nothing to be done for `all'."
-make -j && \
+n=15
+make -j &&
 ./a43 -t multi_grid. \
         +multi_grid.test.scheme=V \
         +multi_grid.test.pre.jacobi=2 \
         +multi_grid.test.post.jacobi=2 \
-        +multi_grid.test.levels=15 \
+        +multi_grid.test.levels=$n \
         +multi_grid.test.min.level=1 \
-        +multi_grid.test.iterations=25 \
+        +multi_grid.test.iterations=30 \
         +multi_grid.test.rhs=sine \
         +verbosity=8 \
-        | grep ' smoothen level=7 2 post' \
-        | awk '{ print $9 }'
+        | grep " smoothen level=$n 2 post" \
+        | awk '{ print $9 }' >> mg.out
+done
 
-# exit
-# 
+# make -j
+# for n in {3..20} 
+# do
+# echo >> mg.out
+# echo "# Number of levels is $n" >> mg.out
 # ./a43 -t multi_grid. \
 #         +multi_grid.test.scheme=V \
 #         +multi_grid.test.pre.jacobi=2 \
 #         +multi_grid.test.post.jacobi=2 \
-#         +multi_grid.test.levels=15 \
+#         +multi_grid.test.levels=$n \
 #         +multi_grid.test.min.level=1 \
-#         +multi_grid.test.iterations=15 \
-#         +multi_grid.test.rhs=random \
-#         +verbosity=8
-#          | grep ' level = 15 after 2 post' | awk '{ print $12 }'
-#         +verbosity=8 | awk '{ print $12 }'   ### show the final residual
-#       +verbosity=8 > multi_grid.out.V_cycle.txt
-##      +verbosity=20 > multi_grid.out.V_cycle.details
+#         +multi_grid.test.iterations=200 \
+#         +multi_grid.test.rhs=sine \
+#         +verbosity=8 \
+#         | grep " smoothen level=$n 2 post" \
+#         | awk '{ print $9 }' >> mg.out
+# done
