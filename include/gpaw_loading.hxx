c6f64d426766d6c226fc579786149c85fa52061d
Paul F Baumeister <p.baumeister@fz-juelich.de>
2022-01-20 21:57:50 +0100

    store parsing results in pawxml_t

--- include/gpaw_loading.hxx
+++ include/gpaw_loading.hxx
@@ -1,7 +1,7 @@
 #pragma once
 
 #include <cstdio> // std::printf
-// #include <cstdint> // int64_t, int32_t, uint32_t, int8_t
+#include <cstdint> // int8_t
 #include <cassert> // assert
 #include <cmath> // std::sqrt
 #include <algorithm> // std::max
@@ -32,45 +32,72 @@
  */
 
 namespace gpaw_loading {
-  
-#ifdef  NO_UNIT_TESTS
-  inline status_t all_tests(int const echo=0) { return STATUS_TEST_NOT_INCLUDED; }
-#else // NO_UNIT_TESTS
 
-  inline status_t test_loading(int const echo=0) {
+  struct pawxmlstate_t {
+      std::vector<double> tsp[3];
+      double e;
+      float f, rc;
+      int8_t n, l;
+  }; // struct pawxmlstate_t 
+
+  struct pawxml_t {
+      double Z, core, valence;
+      double ae_energy[4];
+      double core_energy_kinetic;
+      double radial_grid_a;
+      int    n; // radial_grid.n
+      double shape_function_rc;
+      std::vector<pawxmlstate_t> states;
+      std::vector<double> func[6]; // radial functions
+      std::vector<double> dkin; // kinetic_energy_differences
+      char xc[16];
+      char Sy[4];
+  }; // struct pawxml_t
+
+  char const radial_state_quantities[3][20] = {"ae_partial_wave", "pseudo_partial_wave", "projector_function"};
+
+  char const radial_quantities[6][40] = {"ae_core_density", "pseudo_core_density",
+          "ae_core_kinetic_energy_density", "pseudo_core_kinetic_energy_density", 
+          "zero_potential", "pseudo_valence_density"};
+
+  inline pawxml_t parse_pawxml(char const *filename, int const echo=0) {
 #ifndef HAS_RAPIDXML
       warn("Unable to test GPAW loading when compiled without -D HAS_RAPIDXML", 0);
       return STATUS_TEST_NOT_INCLUDED;
 #else  // HAS_RAPIDXML
-      SimpleTimer timer(__FILE__, __LINE__, __func__, echo);
-      status_t stat(0);
 
-      char const *filename = control::get("gpaw_loading.filename", "C.LDA");
-      if (echo > 0) std::printf("# %s file=%s\n", __func__, filename);
+      pawxml_t p;
+
+      if (echo > 9) std::printf("# %s file=%s\n", __func__, filename);
       rapidxml::file<> infile(filename);
 
       rapidxml::xml_document<> doc;
       doc.parse<0>(infile.data());
 
       auto const paw_setup = doc.first_node("paw_setup");
-      if (!paw_setup) return -1; // error
-      
+      if (!paw_setup) return p; // error
+
       auto const atom = xml_reading::find_child(paw_setup, "atom", echo);
       if (atom) {
           auto const symbol  = xml_reading::find_attribute(atom, "symbol", "?_", echo);
           auto const Z       = xml_reading::find_attribute(atom, "Z",      "-9", echo);
           auto const core    = xml_reading::find_attribute(atom, "core",    "0", echo);
           auto const valence = xml_reading::find_attribute(atom, "valence", "0", echo);
-          if (echo > 0) std::printf("# %s:  <atom symbol=\"%s\" Z=\"%s\" core=\"%s\" valence=\"%s\"/>\n",
+          if (echo > 5) std::printf("# %s:  <atom symbol=\"%s\" Z=\"%s\" core=\"%s\" valence=\"%s\"/>\n",
               filename, symbol, Z, core, valence);
+          // store
+          p.Z       = std::atof(Z);
+          p.core    = std::atof(core);
+          p.valence = std::atof(valence);
+          std::snprintf(p.Sy, 3, "%s", symbol);
       } else warn("no <atom> found in xml-file '%s'", filename);
-      
 
       auto const xc_functional = xml_reading::find_child(paw_setup, "xc_functional", echo);
       if (xc_functional) {
           auto const type = xml_reading::find_attribute(xc_functional, "type", "?type");
           auto const name = xml_reading::find_attribute(xc_functional, "name", "?name");
-          if (echo > 0) std::printf("# %s:  <xc_functional type=\"%s\" name=\"%s\"/>\n", filename, type, name);
+          if (echo > 5) std::printf("# %s:  <xc_functional type=\"%s\" name=\"%s\"/>\n", filename, type, name);
+          std::snprintf(p.xc, 15, "%s", name); // store
       } else warn("no <xc_functional> found in xml-file '%s'", filename);
 
       auto const generator = xml_reading::find_child(paw_setup, "generator", echo);
@@ -78,7 +105,7 @@ namespace gpaw_loading {
           auto const type = xml_reading::find_attribute(generator, "type", "?type");
           auto const name = xml_reading::find_attribute(generator, "name", "?name");
           // caveat: generator->value() contains \n characters
-          if (echo > 0) std::printf("# %s:  <generator type=\"%s\" name=\"%s\"> ... </generator>\n",
+          if (echo > 5) std::printf("# %s:  <generator type=\"%s\" name=\"%s\"> ... </generator>\n",
               filename, type, name);
       } else warn("no <generator> found in xml-file '%s'", filename);
 
@@ -88,18 +115,23 @@ namespace gpaw_loading {
           auto const xc            = xml_reading::find_attribute(ae_energy, "xc",            "0");
           auto const electrostatic = xml_reading::find_attribute(ae_energy, "electrostatic", "0");
           auto const total         = xml_reading::find_attribute(ae_energy, "total",         "0");
-          if (echo > 0) std::printf("# %s:  <ae_energy kinetic=\"%s\" xc=\"%s\" electrostatic=\"%s\" total=\"%s\"/>\n",
+          if (echo > 5) std::printf("# %s:  <ae_energy kinetic=\"%s\" xc=\"%s\" electrostatic=\"%s\" total=\"%s\"/>\n",
               filename, kinetic, xc, electrostatic, total);
+          // store
+          p.ae_energy[0] = std::atof(kinetic);
+          p.ae_energy[1] = std::atof(xc);
+          p.ae_energy[2] = std::atof(electrostatic);
+          p.ae_energy[3] = std::atof(total);
       } else warn("no <ae_energy> found in xml-file '%s'", filename);
 
       auto const core_energy = xml_reading::find_child(paw_setup, "core_energy", echo);
       if (core_energy) {
           auto const kinetic = xml_reading::find_attribute(core_energy, "kinetic",       "0");
-          if (echo > 0) std::printf("# %s:  <core_energy kinetic=\"%s\"/>\n", filename, kinetic);
+          if (echo > 5) std::printf("# %s:  <core_energy kinetic=\"%s\"/>\n", filename, kinetic);
+          p.core_energy_kinetic = std::atof(kinetic); // store
       } else warn("no <core_energy> found in xml-file '%s'", filename);
 
-
-      int ngrid{0};
+      p.n = 0;
       auto const radial_grid = xml_reading::find_child(paw_setup, "radial_grid", echo);
       if (radial_grid) {
           auto const eq     = xml_reading::find_attribute(radial_grid, "eq", "?eq");
@@ -108,18 +140,21 @@ namespace gpaw_loading {
           auto const istart = xml_reading::find_attribute(radial_grid, "istart", "0");
           auto const iend   = xml_reading::find_attribute(radial_grid, "iend", "-1");
           auto const id     = xml_reading::find_attribute(radial_grid, "id", "?");
-          if (echo > 0) std::printf("# %s:  <radial_grid eq=\"%s\" a=\"%s\" n=\"%s\" istart=\"%s\" iend=\"%s\" id=\"%s\"/>\n",
+          if (echo > 5) std::printf("# %s:  <radial_grid eq=\"%s\" a=\"%s\" n=\"%s\" istart=\"%s\" iend=\"%s\" id=\"%s\"/>\n",
               filename, eq, a, n, istart, iend, id);
-          ngrid = std::atoi(n); // convert to integer
+          p.n = std::atoi(n); // store
+          p.radial_grid_a = std::atof(a); // store
+          if (0 != std::strcmp(eq, "r=a*i/(n-i)")) error("%s: assume a radial expontential grid", filename);
+          if (0 != std::atoi(istart))              error("%s: assume a radial grid starting at 0", filename);
+          if (p.n != std::atoi(iend) + 1)          error("%s: assume a radial grid starting from 0 to n-1", filename);
       } else warn("no <radial_grid> found in xml-file '%s'", filename);
 
-      char const *radial_state_quantities[20] = {"ae_partial_wave", "pseudo_partial_wave", "projector_function"};
-
-      int nstates{0};
+      int istate{0};
+      p.states.resize(0);
       int nwarn[] = {0, 0, 0};
       auto const valence_states = xml_reading::find_child(paw_setup, "valence_states", echo);
       if (valence_states) {
-          if (echo > 0) std::printf("# %s:  <valence_states>\n", filename);
+          if (echo > 5) std::printf("# %s:  <valence_states>\n", filename);
           for (auto state = valence_states->first_node(); state; state = state->next_sibling()) {
               auto const n  = xml_reading::find_attribute(state, "n",  "0");
               auto const l  = xml_reading::find_attribute(state, "l", "-1");
@@ -127,8 +162,15 @@ namespace gpaw_loading {
               auto const rc = xml_reading::find_attribute(state, "rc", "0");
               auto const e  = xml_reading::find_attribute(state, "e",  "0");
               auto const id = xml_reading::find_attribute(state, "id", "?");
-              if (echo > 0) std::printf("# %s:    <state n=\"%s\" l=\"%s\" f=\"%s\" rc=\"%s\" e=\"%s\" id=\"%s\"/>\n",
+              if (echo > 5) std::printf("# %s:    <state n=\"%s\" l=\"%s\" f=\"%s\" rc=\"%s\" e=\"%s\" id=\"%s\"/>\n",
                   filename, n, l, f, rc, e, id);
+
+              pawxmlstate_t s;
+              s.n  = std::atoi(n);
+              s.l  = std::atoi(l);
+              s.f  = std::atof(f);
+              s.rc = std::atof(rc);
+              s.e  = std::atof(e);
 
               for (int iq = 0; iq < 3; ++iq) {
                   auto const q_name = radial_state_quantities[iq];
@@ -138,29 +180,30 @@ namespace gpaw_loading {
                           auto const state_id = xml_reading::find_attribute(child, "state", "?state");
                           if (0 == std::strcmp(state_id, id)) {
                               auto const grid = xml_reading::find_attribute(child, "grid", "?grid");
-                              auto const vals = xml_reading::read_sequence<double>(child->value(), echo, ngrid);
-                              if (echo > 0) std::printf("# %s:  <%s state=\"%s\" grid=\"%s\"> ...(%ld numbers)... </%s>\n",
+                              auto const vals = xml_reading::read_sequence<double>(child->value(), echo, p.n);
+                              if (echo > 7) std::printf("# %s:  <%s state=\"%s\" grid=\"%s\"> ...(%ld numbers)... </%s>\n",
                                   filename, q_name, state_id, grid, vals.size(), q_name);
-                              nwarn[iq] += (vals.size() != ngrid);
+                              nwarn[iq] += (vals.size() != p.n);
+                              s.tsp[iq] = vals;
                               ++radial_data_found;
                           } // state_id matches
                       } // found
                   } // attr
-                  if (1 != radial_data_found) {
-                      error("%s: radial state quantities in pawxml files must be defined exactly once!", filename);
-                      ++stat;
-                  } // found more or less than once
+                  if (1 != radial_data_found) error("%s: radial state quantities in pawxml files must be defined exactly once!", filename);
               } // iq
-              ++nstates;
+              p.states.push_back(s); // store
+              ++istate;
 
           } // state
-          if (echo > 0) std::printf("# %s:  </valence_states>\n", filename);
+          if (echo > 5) std::printf("# %s:  </valence_states>\n", filename);
       } else warn("no <valence_states> found in xml-file '%s'", filename);
+      int const nstates = istate;
+      assert(nstates == p.states.size());
 
       for (int iq = 0; iq < 3; ++iq) {
           if (nwarn[iq]) {
               warn("%s: %d %s deviate from the expected number of %d grid points",
-                   filename, nwarn[iq], radial_state_quantities[iq], ngrid);
+                   filename, nwarn[iq], radial_state_quantities[iq], p.n);
           } // warn
       } // iq
 
@@ -168,37 +211,113 @@ namespace gpaw_loading {
       if (shape_function) {
           auto const type = xml_reading::find_attribute(shape_function, "type", "?type");
           auto const rc   = xml_reading::find_attribute(shape_function, "rc", "0");
-          if (echo > 0) std::printf("# %s:  <shape_function type=\"%s\" rc=\"%s\"/>\n", filename, type, rc);
+          if (echo > 5) std::printf("# %s:  <shape_function type=\"%s\" rc=\"%s\"/>\n", filename, type, rc);
+          if (std::strcmp(type, "gauss")) error("%s: assume a shape_function type gauss", filename);
+          p.shape_function_rc = std::atof(rc); // store
       } else warn("no <shape_function> found in xml-file '%s'", filename);
 
-      auto const radial_quantities = {"ae_core_density", "pseudo_core_density",
-          "ae_core_kinetic_energy_density", "pseudo_core_kinetic_energy_density", 
-          "zero_potential"}; // , "pseudo_valence_density"};
-      for (auto q_name : radial_quantities) {
+      for (int iq = 0; iq < 5; ++iq) {
+          auto const q_name = radial_quantities[iq];
           auto const q_node = xml_reading::find_child(paw_setup, q_name, echo);
           if (q_node) {
               auto const grid = xml_reading::find_attribute(q_node, "grid", "?grid");
-              auto const vals = xml_reading::read_sequence<double>(q_node->value(), echo, ngrid);
-              if (echo > 0) std::printf("# %s:  <%s grid=\"%s\"> ...(%ld numbers)... </%s>\n",
+              auto const vals = xml_reading::read_sequence<double>(q_node->value(), echo, p.n);
+              if (echo > 6) std::printf("# %s:  <%s grid=\"%s\"> ...(%ld numbers)... </%s>\n",
                   filename, q_name, grid, vals.size(), q_name);
-              if (vals.size() != ngrid) warn("%s: %s has %ld but expected %d grid points",
-                   filename, q_name, vals.size(), ngrid);
+              if (vals.size() != p.n) warn("%s: %s has %ld but expected %d grid points",
+                   filename, q_name, vals.size(), p.n);
+              p.func[iq] = vals; // store
           } else warn("no <%s> found in xml-file '%s'", q_name, filename);
-      } // q_name
+      } // iq
 
       auto const kinetic_energy_differences = xml_reading::find_child(paw_setup, "kinetic_energy_differences", echo);
       if (kinetic_energy_differences) {
           auto const vals = xml_reading::read_sequence<double>(kinetic_energy_differences->value(), echo, nstates*nstates);
-          if (echo > 0) std::printf("# %s:  <kinetic_energy_differences> ...(%ld numbers)... </kinetic_energy_differences>\n", filename, vals.size());
+          if (echo > 5) std::printf("# %s:  <kinetic_energy_differences> ...(%ld numbers)... </kinetic_energy_differences>\n", filename, vals.size());
           if (vals.size() != nstates*nstates) warn("%s: expected %d^2 numbers in kinetic_energy_differences matrix but found %ld", filename, nstates, vals.size());
+          p.dkin = vals; // store
       } else warn("no <kinetic_energy_differences> found in xml-file '%s'", filename);
 
 // not covert:      
 //   <exact_exchange_X_matrix>  </exact_exchange_X_matrix>
 //   <exact_exchange core-core="-3.462071"/>
 
-      return stat;
+      return p;
 #endif // HAS_RAPIDXML
+  } // parse_pawxml
+
+
+      
+#ifdef  NO_UNIT_TESTS
+  inline status_t all_tests(int const echo=0) { return STATUS_TEST_NOT_INCLUDED; }
+#else // NO_UNIT_TESTS
+
+  inline status_t test_loading(int const echo=0) {
+      SimpleTimer timer(__FILE__, __LINE__, __func__, echo);
+
+      char const *filename = control::get("gpaw_loading.test.filename", "C.LDA");
+      if (echo > 7) std::printf("# %s file=%s\n", __func__, filename);
+      auto const p = parse_pawxml(filename, echo);
+
+      auto const repeat_file = control::get("gpaw_loading.test.repeat", 0.0);
+      if (repeat_file) {
+          // create an XML file that should reproduce this
+          char outfile[992]; std::snprintf(outfile, 991, "%s.repeat.xml", filename);
+          auto const f = std::fopen(outfile, "w");
+          std::fprintf(f, "<?xml version=\"1.0\"?>\n"
+              "<paw_setup version=\"0.6\">\n"
+              "\t<!-- Element setup for the Projector Augmented Wave method -->\n"
+              "\t<!-- Units: Hartree and Bohr radii.                        -->\n");
+          std::fprintf(f, "\t<atom symbol=\"%s\" Z=\"%g\" core=\"%g\" valence=\"%g\"/>\n", p.Sy, p.Z, p.core, p.valence);
+          std::fprintf(f, "\t<xc_functional name=\"%s\"/>\n", p.xc);
+          std::fprintf(f, "\t<ae_energy kinetic=\"%.6f\" xc=\"%.6f\" electrostatic=\"%.6f\" total=\"%.6f\"/>\n",
+                              p.ae_energy[0], p.ae_energy[1], p.ae_energy[2], p.ae_energy[3]);
+          std::fprintf(f, "\t<core_energy kinetic=\"%.6f\"/>\n", p.core_energy_kinetic);
+          std::fprintf(f, "\t<valence_states>\n");
+          char const ellchar[8] = "spdfghi";
+          for (auto & s : p.states) {
+              char id[8]; std::snprintf(id, 7, "%s-%d%c", p.Sy, s.n, ellchar[s.l]);
+              std::fprintf(f, "\t\t<state n=\"%d\" l=\"%d\" f=\"%g\" rc=\"%g\" e=\"%g\" id=\"%s\"/>\n",
+                                        s.n,     s.l,     s.f,     s.rc,     s.e,       id);
+          } // states
+          std::fprintf(f, "\t</valence_states>\n");
+          std::fprintf(f, "\t<radial_grid eq=\"r=a*i/(n-i)\" a=\"%g\" n=\"%d\" istart=\"0\" iend=\"%d\" id=\"g1\"/>\n",
+                                               p.radial_grid_a,     p.n,                     p.n - 1);
+          std::fprintf(f, "\t<shape_function type=\"gauss\" rc=\"%.12e\"/>\n", p.shape_function_rc);
+
+          // radial_quantities
+          for (int iq = 0; iq < 5; ++iq) {
+              std::fprintf(f, "\t<%s grid=\"g1\">\n", radial_quantities[iq]);
+              for (auto val : p.func[iq]) {
+                  std::fprintf(f, " %g", val);
+              } // val
+              std::fprintf(f, "\n\t</%s>\n", radial_quantities[iq]);
+          } // iq
+          
+          // radial_state_quantities
+          for (auto & s : p.states) {
+              char id[8]; std::snprintf(id, 7, "%s-%d%c", p.Sy, s.n, ellchar[s.l]);
+              for (int iq = 0; iq < 3; ++iq) {
+                  std::fprintf(f, "\t<%s state=\"%s\" grid=\"g1\">\n", radial_state_quantities[iq], id);
+                  for (auto val : s.tsp[iq]) {
+                      std::fprintf(f, " %g", val);
+                  } // val
+                  std::fprintf(f, "\n\t</%s>\n", radial_state_quantities[iq]);
+              } // iq
+          } // s
+
+          std::fprintf(f, "\t<kinetic_energy_differences>");
+          for (int i = 0; i < p.states.size(); ++i) {
+              for (int j = 0; j < p.states.size(); ++j) {
+                  std::fprintf(f, "%s%g", j?" ":"\n\t\t", p.dkin[i*p.states.size() + j]);
+              } // j
+          } // i
+          std::fprintf(f, "\n\t</kinetic_energy_differences>\n");
+          std::fprintf(f, "</paw_setup>\n");
+          std::fclose(f);
+      } // repeat_file
+
+      return 0;
   } // test_loading
 
   inline status_t all_tests(int const echo=0) {
