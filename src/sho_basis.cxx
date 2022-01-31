4aebc33f4fd8b143f7b63b39ba311b34422390e4
Paul F Baumeister <p.baumeister@fz-juelich.de>
2022-01-31 15:10:50 +0100

    workaround memory corruption problem appearing when plotting

--- src/sho_basis.cxx
+++ src/sho_basis.cxx
@@ -64,9 +64,15 @@ namespace sho_basis {
       std::vector<std::vector<std::vector<double>>> all_coeffs;
       std::vector<double> all_sigma;
       std::vector<int> all_numax;
-
+      std::vector<std::vector<int8_t>> all_ells, all_enns;
+      
       // check all bases
       for (auto atomic = main_node->first_node("atomic"); atomic; atomic = atomic->next_sibling()) {
+//        if (echo > 0) std::printf("# %s atomic= %p\n", __func__, (void*)atomic);
+//           for (auto attr = atomic->first_attribute(); attr; attr = attr->next_attribute()) {
+//               if (echo > 0) std::printf("# %s atomic= %p attribute= %s value= %s\n",
+//                             __func__, (void*)atomic, attr->name(), attr->value());
+//           } // attr
           auto const symbol =          (xml_reading::find_attribute(atomic, "symbol", "?"));
           auto const Z      = std::atof(xml_reading::find_attribute(atomic, "Z",     "-9"));
           auto const numax  = std::atoi(xml_reading::find_attribute(atomic, "numax", "-1"));
@@ -80,15 +86,16 @@ namespace sho_basis {
               int const ell = std::atoi(xml_reading::find_attribute(wave, "l", "-1"));
               assert(ell >= 0);
               assert(enn > ell);
-              int const n_expect = sho_tools::nn_max(numax, ell);
+              size_t const n_expect = sho_tools::nn_max(numax, ell);
               auto const vec = xml_reading::read_sequence<double>(wave->value(), echo, n_expect);
-              if (echo > 0) std::printf("# %s   %s-%d%c  (%ld of %d elements)\n", __func__, symbol, enn, ellchar[ell], vec.size(), n_expect);
+              if (echo > 0) std::printf("# %s   %s-%d%c  (%ld of %ld elements)\n", __func__, symbol, enn, ellchar[ell], vec.size(), n_expect);
               assert(n_expect == vec.size());
               enns.push_back(enn);
               ells.push_back(ell);
               coeffs.push_back(vec);
           } // wave
 
+#if 0
           if (coeffs.size() > 0) { // plot
               assert(coeffs.size() == enns.size());
               assert(coeffs.size() == ells.size());
@@ -100,10 +107,60 @@ namespace sho_basis {
                   scattering_test::expand_sho_projectors(basis_funcs.data(), basis_funcs.stride(), rg, sigma, numax, 0, 17);
                   std::printf("\n## radius ");
                   for (int i = 0; i < coeffs.size(); ++i) {
+                      assert(ells[i] >= 0);
                       std::printf(" %d%c", enns[i], ellchar[ells[i]]);
                       assert(coeffs[i].size() == sho_tools::nn_max(numax, ells[i]));
                   } // i
-                  if (echo > 30) { // ToDo: remove
+                  if (echo > 30) { // ToDo: remove this condition
+                  std::printf(" :\n");
+                  for (int ir = 0; ir < rg.n; ++ir) {
+                      std::printf("%g", rg.r[ir]);
+                      for (int i = 0; i < coeffs.size(); ++i) {
+                          int const iln0 = sho_tools::ln_index(numax, ells[i], 0);
+                          double wave{0};
+                          for (int jrn = 0; jrn < coeffs[i].size(); ++jrn) {
+                              wave += coeffs[i][jrn] * basis_funcs(iln0 + jrn,ir);
+                          } // jrn
+                          std::printf(" %g", wave);
+                      } // i
+                      std::printf("\n");
+                  } // ir
+                  } // echo > 30
+                  std::printf("\n\n");
+              } // echo
+          } // plot
+#endif
+          
+          all_coeffs.push_back(coeffs);
+          all_numax.push_back(numax);
+          all_sigma.push_back(sigma);
+          all_ells.push_back(ells);
+          all_enns.push_back(enns);
+
+//        if (echo > 0) std::printf("# %s atomic->next_sibling= %p\n", __func__, (void*)atomic->next_sibling());
+      } // atomic
+
+      
+      for (int k = 0; k < all_coeffs.size(); ++k) {
+          auto const & coeffs = all_coeffs[k];
+          auto const numax  = all_numax[k];
+          auto const sigma  = all_sigma[k];
+          auto const & ells = all_ells[k];
+          auto const & enns = all_enns[k];
+
+          if (coeffs.size() > 0) { // plot
+              assert(coeffs.size() == enns.size());
+              assert(coeffs.size() == ells.size());
+              if (echo > 7) {
+                  int const nln = sho_tools::nSHO_radial(numax);
+                  view2D<double> basis_funcs(nln, rg.n, 0.0);
+                  scattering_test::expand_sho_projectors(basis_funcs.data(), basis_funcs.stride(), rg, sigma, numax);
+                  std::printf("\n## radius ");
+                  for (int i = 0; i < coeffs.size(); ++i) {
+                      assert(ells[i] >= 0);
+                      std::printf(" %d%c", enns[i], ellchar[ells[i]]);
+                      assert(coeffs[i].size() == sho_tools::nn_max(numax, ells[i]));
+                  } // i
                   std::printf(" :\n");
                   for (int ir = 0; ir < rg.n; ++ir) {
                       std::printf("%g", rg.r[ir]);
@@ -118,14 +175,13 @@ namespace sho_basis {
                       std::printf("\n");
                   } // ir
                   std::printf("\n\n");
-                  } // echo > 30
               } // echo
           } // plot
-
-      } // atomic
+          
+      } // k
       
       radial_grid::destroy_radial_grid(&rg);
-      
+
       return 0;
 #endif // HAS_RAPIDXML
   }
