a6dd4d5dba83b263faeb857b6ea0d771cb51235b
Paul F Baumeister <p.baumeister@fz-juelich.de>
2022-07-26 14:30:16 +0200

    take plane balancer to a function

--- src/load_balancer.cxx
+++ src/load_balancer.cxx
@@ -82,6 +82,176 @@ namespace load_balancer {
   // 3) assume that the largest extent is along the line through the center of weight and that position
   // --> a cuboid will always use the space diagonal
 
+  template <typename real_t>
+  inline double plane_balancer(
+        int const nprocs
+      , int const rank
+      , size_t const nall
+      , view2D<real_t> const & xyzw
+      , real_t const w8s[]
+      , double const w8sum_all=1.
+      , double rank_center[4]=nullptr
+      , int const echo=0
+  ) {
+      int constexpr W = 3;
+
+      bool constexpr UNASSIGNED = 0, ASSIGNED = 1;
+      std::vector<bool> state(nall, UNASSIGNED);
+
+      std::vector<uint32_t> indirect(nall, 0);
+      std::iota(indirect.begin(), indirect.end(), 0); // initialize with 0,1,....,nall-1
+
+      size_t nuna{nall}; // number of unassigned blocks
+
+      double load_now{w8sum_all};
+
+      int np{nprocs}; // current number of processes among which we distribute
+      int rank_offset{0}; // offset w.r.t. MPI ranks
+
+      while (np > 1) {
+
+          assert(rank_offset + np <= nprocs);
+          // bisect the workload for np processors into (np + 1)/2 and np/2
+          int const nhalf[] = {(np + 1) >> 1, np >> 1};
+          // MPIrank ranges are {off ... off+nhalf[0]-1} and {off+nhalf[0] ...off+np-1}
+          int const i01 = (rank >= rank_offset + nhalf[0]);
+          // i01 == 0: this rank is part of the first (np + 1)/2 processes
+          // i01 == 1: this rank is part of the second np/2 processes
+          if (echo > 19) std::printf("# rank#%i divides %d into %d and %d\n", rank, np, nhalf[i01], nhalf[1 - i01]);
+          assert(nhalf[0] + nhalf[1] == np);
+
+          // determine the direction of the largest extent
+
+          // determine the center of weight
+          double cow[] = {0, 0, 0, 0};
+          double w8sum{0};
+          for (size_t iuna = 0; iuna < nuna; ++iuna) {
+              auto const iall = indirect[iuna];
+              auto const *const xyz = xyzw[iall];
+              double const w8 = xyz[W];
+//            assert(w8 == w8s[iall]);
+              w8sum += w8;
+              // contributes if the weight is positive
+              double const w8pos = double(w8 > 0);
+              add_product(cow, 3, xyz, w8pos);
+              cow[3] += w8pos;
+          } // iall
+          if (cow[3] > 0) scale(cow, 3, 1./cow[3]);
+
+          // determine the largest distance^2 from the center
+          double maxdist2{-1};
+          int64_t imax{-1}; // index of the block with the largest distance
+          for (size_t iuna = 0; iuna < nuna; ++iuna) {
+              auto const iall = indirect[iuna];
+              auto const *const xyz = xyzw[iall];
+              auto const dist2 = pow2(xyz[X] - cow[X])
+                               + pow2(xyz[Y] - cow[Y])
+                               + pow2(xyz[Z] - cow[Z]);
+              if (dist2 > maxdist2) {
+                  maxdist2 = dist2;
+                  imax = iall;
+              } // dist2 > maxdist2
+          } // iuna
+
+          if (imax < 0) {
+              // this should only happens if the process is idle,
+              //   i.e. there are no blocks assigned to this one
+              load_now = 0;
+              np = 1; // stop the while-loop
+          } else {
+
+              // determine a sorting direction
+              add_product(cow, 3, xyzw[imax], -1.);
+              auto const len2 = pow2(cow[X]) + pow2(cow[Y]) + pow2(cow[Z]);
+              auto const norm = (len2 > 0) ? 1./std::sqrt(len2) : 0.0;
+              double const vec[] = {cow[X]*norm, cow[Y]*norm, cow[Z]*norm};
+              if (echo > 19) std::printf("# rank#%i sort along the [%g %g %g] direction\n", rank, vec[X], vec[Y], vec[Z]);
+
+              using fui_t = std::pair<float,uint32_t>;
+              std::vector<fui_t> v(nuna);
+
+              for (size_t iuna = 0; iuna < nuna; ++iuna) {
+                  auto const iall = indirect[iuna];
+                  auto const *const xyz = xyzw[iall];
+                  auto const f = xyz[X]*vec[X] + xyz[Y]*vec[Y] + xyz[Z]*vec[Z]; // inner product
+                  v[iuna] = std::make_pair(float(f), uint32_t(iall));
+              } // iall
+
+              auto lambda = [](fui_t i1, fui_t i2) { return i1.first < i2.first; };
+              std::stable_sort(v.begin(), v.end(), lambda);
+
+              auto const target_frac = nhalf[0]/double(np), // the "larger" half
+                         target_load = target_frac*w8sum;
+              {
+                  auto const state0 = i01 ? ASSIGNED : UNASSIGNED,
+                             state1 = i01 ? UNASSIGNED : ASSIGNED;
+                  double load0{0}, load1{0};
+                  size_t isrt;
+                  for (isrt = 0; load0 < target_load; ++isrt) {
+                      auto const iall = v[isrt].second;
+                      load0 += w8s[iall];
+                      state[iall] = state0;
+                  }
+                  for(; isrt < nuna; ++isrt) {
+                      auto const iall = v[isrt].second;
+                      load1 += w8s[iall];
+                      state[iall] = state1;
+                  } // i
+                  assert(load0 + load1 == w8sum && "Maybe failed due to accuracy issues");
+                  load_now = i01 ? load1 : load0;
+              }
+
+              if (echo > 19) std::printf("# rank#%i assign %g of %g (%.2f %%, target %.2f %%) to %d processes\n", 
+                                          rank, load_now, w8sum, load_now*100./w8sum, target_frac*100, nhalf[0]);
+
+              // prepare for the next iteration
+              rank_offset += i01*nhalf[0];
+              np = nhalf[i01];
+              // update the indirection list
+              // determine which blocks are still unassigned
+              size_t iunk{0}; // counter
+              for (size_t iuna = 0; iuna < nuna; ++iuna) { // serial loop due to read-write access to array indirect
+                  auto const iall = indirect[iuna]; // read from array indirect
+                  if (UNASSIGNED == state[iall]) {
+                      indirect[iunk] = iall; // write to array indirect at a lower address
+                      ++iunk;
+                  } // is_unassigned
+              } // iall
+              assert(iunk <= nuna);
+              nuna = iunk; // set new number of unassigned blocks
+
+          } // imax < 0
+
+      } // while np > 1
+
+      if (rank_center && load_now > 0) {
+          // compute the center of weight again, for display
+          double cow[] = {0, 0, 0, 0}, w8sum{0};
+          for (size_t iuna = 0; iuna < nuna; ++iuna) {
+              auto const iall = indirect[iuna];
+              auto const *const xyz = xyzw[iall];
+              double const w8 = xyz[W];
+              assert(w8 == w8s[iall]);
+              w8sum += w8;
+              // contributes if the weight is positive
+              double const w8pos = double(w8 > 0);
+              add_product(cow, 3, xyz, w8pos);
+              cow[3] += w8pos;
+          } // iall
+          if (cow[3] > 0) scale(cow, 3, 1./cow[3]);
+          set(rank_center, 4, cow);
+          if (echo > 13) std::printf("# rank#%i assign %.3f %% center %g %g %g\n", 
+                                        rank, w8sum*100/w8sum_all, cow[X], cow[Y], cow[Z]);
+      } // rank_center
+
+      if (echo > 9) std::printf("# rank#%i assign %.3f %%, target %.3f %%\n\n", 
+                                   rank, load_now*100/w8sum_all, 100./nprocs);
+
+      return load_now;
+  } // plane_balancer
+
+
+
   inline status_t test_plane_balancer(int const nprocs, int const n[3], int const echo=0) {
       status_t stat(0);
 
@@ -117,6 +287,7 @@ namespace load_balancer {
 
 for (int rank = 0; rank < nprocs; ++rank) {
 
+#if 0
 
       bool constexpr UNASSIGNED = 0, ASSIGNED = 1;
       std::vector<bool> state(nall, UNASSIGNED);
@@ -270,7 +441,9 @@ for (int rank = 0; rank < nprocs; ++rank) {
       if (echo > 9) std::printf("# rank#%i assign %.3f %%, target %.3f %%\n\n", 
                                    rank, load_now*100/w8sum_all, 100./nprocs);
       load[rank] = load_now;
-
+#else
+      load[rank] = plane_balancer(nprocs, rank, nall, xyzw, w8s.data(), w8sum_all, rank_center[rank], echo);
+#endif
 } // rank
 
 
@@ -280,13 +453,17 @@ for (int rank = 0; rank < nprocs; ++rank) {
           // analyze positions of rank centers
           double mindist2{9e300}; int ijmin[] = {-1, -1};
           double maxdist2{-1.0};  int ijmax[] = {-1, -1};
+          simple_stats::Stats<> st2, st1;
           for (int irank = 0; irank < nprocs; ++irank) {
               if (load[irank] > 0) {
-                  for (int jrank = 0; jrank < irank; ++jrank) { // self-avoiding triangular loop
+                  for (int jrank = 0; jrank < nprocs; ++jrank) { // self-avoiding triangular loop
                       if (load[jrank] > 0) {
                           auto const dist2 = distance_squared(rank_center[irank], rank_center[jrank]);
-                          if (dist2 < mindist2) { mindist2 = dist2; ijmin[0] = irank; ijmin[1] = jrank; }
+                          if (dist2 > 0 && dist2 < mindist2) { mindist2 = dist2; ijmin[0] = irank; ijmin[1] = jrank; }
                           if (dist2 > maxdist2) { maxdist2 = dist2; ijmax[0] = irank; ijmax[1] = jrank; }
+                          auto const dist = std::sqrt(dist2);
+                          st2.add(dist2);
+                          st1.add(dist);
                       } // load
                   } // jrank
               } // load
@@ -301,7 +478,7 @@ for (int rank = 0; rank < nprocs; ++rank) {
           for (int irank = 0; irank < nprocs; ++irank) {
               if (load[irank] > 0) {
                   ++np;
-                  for (int jrank = 0; jrank < irank; ++jrank) { // self-avoiding triangular loop
+                  for (int jrank = 0; jrank < nprocs; ++jrank) { // self-avoiding triangular loop
                       if (load[jrank] > 0) {
                           auto const dist2 = distance_squared(rank_center[irank], rank_center[jrank]);
                           auto const dist = std::sqrt(dist2);
@@ -320,6 +497,10 @@ for (int rank = 0; rank < nprocs; ++rank) {
               } // ibin
               std::printf("\n\n");
           } // echo
+          if (echo > 2) std::printf("# stats: distance [%g, %g +/- %g, %g]\n"
+                                    "#        distance^2 [%g, %g +/- %g, %g]\n",
+                                    st1.min(), st1.mean(), st1.dev(), st1.max(),
+                                    st2.min(), st2.mean(), st2.dev(), st2.max());
 
       } // compute_rank_centers
 
@@ -345,21 +526,25 @@ for (int rank = 0; rank < nprocs; ++rank) {
           int const nbin = int(maxdist_diagonal/wbin) + 1;
           std::vector<uint32_t> hist(nbin, 0);
           double mindist2{9e300}, maxdist2{-1.0};
+          simple_stats::Stats<> st2, st1;
           for (int iz = 0; iz < nxyz[Z]; ++iz) {
           for (int iy = 0; iy < nxyz[Y]; ++iy) {
           for (int ix = 0; ix < nxyz[X]; ++ix) {
                   for (int jz = 0; jz < nxyz[Z]; ++jz) {
                   for (int jy = 0; jy < nxyz[Y]; ++jy) {
                   for (int jx = 0; jx < nxyz[X]; ++jx) {
-                          double const rnd[] = {0.5*random_between_0_and_1() - 0.25,
-                                                0.5*random_between_0_and_1() - 0.25,
-                                                0.5*random_between_0_and_1() - 0.25};
+//                           double const rnd[] = {0.5*random_between_0_and_1() - 0.25,
+//                                                 0.5*random_between_0_and_1() - 0.25,
+//                                                 0.5*random_between_0_and_1() - 0.25};
+                          int constexpr rnd[] = {0, 0, 0};
                           double const dist2 = pow2(ix - jx + rnd[X])
                                              + pow2(iy - jy + rnd[Y])
                                              + pow2(iz - jz + rnd[Z]);
                           mindist2 = std::min(mindist2, dist2);
                           maxdist2 = std::max(maxdist2, dist2);
                           auto const dist = std::sqrt(dist2);
+                          st2.add(dist2);
+                          st1.add(dist);
                           if (echo > 15) std::printf("# distance-ij is %g\n", dist);
                           int const ibin = dist*invbin; // floor
                           ++hist[ibin];
@@ -370,17 +555,21 @@ for (int rank = 0; rank < nprocs; ++rank) {
                                         std::sqrt(mindist2), maxdist);
           if (echo > 5) {
               double const by_n = 1./(1.*nxyz[X]*nxyz[Y]*nxyz[Z]);
-              double const denom = 0.5*pow2(by_n);
+              double const denom = pow2(by_n);
               std::printf("## point-distance histogram, bin width %g\n", wbin);
               for (int ibin = 0; ibin < nbin; ++ibin) {
                   auto const radius = ibin*wbin;
                   // number of points inside a sphere shell of from radius r - wbin to r is 4/3*pi*(r^3 - (r - wbin)^3)*rho
                   // so in the limit of small wbin, this becomes 4*pi*r^2*wbin*rho
-                  auto const analytical = 2*constants::pi*pow2(radius)*wbin*by_n;
+                  auto const analytical = 4*constants::pi*pow2(radius)*wbin*by_n;
                   std::printf("%g %g %g\n", radius, hist[ibin]*denom, analytical);
               } // ibin
               std::printf("\n\n");
           } // echo
+          if (echo > 2) std::printf("# stats: distance [%g, %g +/- %g, %g]\n"
+                                    "#        distance^2 [%g, %g +/- %g, %g]\n",
+                                    st1.min(), st1.mean(), st1.dev(), st1.max(),
+                                    st2.min(), st2.mean(), st2.dev(), st2.max());
           return 0;
   } // test_reference_point_cloud
 
@@ -397,7 +586,7 @@ for (int rank = 0; rank < nprocs; ++rank) {
                       __func__, nxyz[X], nxyz[Y], nxyz[Z], nxyz[X]*nxyz[Y]*nxyz[Z], nprocs);
 
       stat += test_plane_balancer(nprocs, nxyz, echo);
-//    stat += test_reference_point_cloud(nxyz, echo);
+//       stat += test_reference_point_cloud(nxyz, echo);
       return stat;
   } // all_tests
 
