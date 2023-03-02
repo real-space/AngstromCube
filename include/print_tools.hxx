795f911eedd731d39ce5ef4a4b4713786bc6afaf
Paul F Baumeister <p.baumeister@fz-juelich.de>
2023-03-02 22:58:33 +0100

    linter more

--- include/print_tools.hxx
+++ include/print_tools.hxx
@@ -2,18 +2,20 @@
 
 #include <cstdio> // std::printf
 #include <vector> // std::vector<T>
+#include <algorithm> // std::min, ::max
+
+#include "inline_math.hxx" // pow2
 
   template <typename T>
   int printf_vector( // returns the total number of chars written
         char const *const format // printf-format, should contain '%' only once
-      , T const *const vec // pointer to vector start
+      , T const vec[] // pointer to vector start
       , int const n // number of elements to write
       , char const *const final="\n"
       , T const scale=T(1) // scale the vector components
       , T const add=T(0)   // add to the scaled vector components
   ) {
       // write a vector with increment +1 to stdout
-
       int n_chars_written{0};
       for (int i{0}; i < n; ++i) {
           n_chars_written += std::printf(format, vec[i]*scale + add);
@@ -39,13 +41,13 @@
       , size_t const all // how many
       , double const dV=1 // volume element for the integration
       , char const *prefix="" // leading printf messages
-      , real_t const unit=1 // unit conversion factor
+      , double const unit=1 // unit conversion factor
       , char const *_unit="" // unit indicator
   ) {
-      real_t gmin{9e307}, gmax{-gmin}; double gsum{0}, gsum2{0};
+      double gmin{9e307}, gmax{-gmin}, gsum{0}, gsum2{0};
       for (size_t i = 0; i < all; ++i) {
-          gmin = std::min(gmin, values[i]);
-          gmax = std::max(gmax, values[i]);
+          gmin = std::min(gmin, double(values[i]));
+          gmax = std::max(gmax, double(values[i]));
           gsum  += values[i];
           gsum2 += pow2(values[i]);
       } // i
