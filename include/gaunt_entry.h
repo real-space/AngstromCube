97b824a346b3cde32fb8a4b21867a98e1fe18bc0
Paul F Baumeister <p.baumeister@fz-juelich.de>
2023-03-02 22:51:30 +0100

    linter

--- include/gaunt_entry.h
+++ include/gaunt_entry.h
@@ -6,12 +6,11 @@
 extern "C" {
 #endif
 
-
-typedef struct {
-  double G;
-  int32_t lm;
-  int16_t lm1, lm2;
-} gaunt_entry_t;
+  typedef struct {
+      double G; // Gaunt coefficient
+      int32_t lm; // combined index ell0 emm0
+      int16_t lm1, lm2; // combined indices ell1 emm1 and ell2 emm2
+  } gaunt_entry_t;
 
 #ifdef __cplusplus
 } // extern "C"
