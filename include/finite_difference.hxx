8692eab27cd7a02850833ef2374d595b604d910e
Paul F Baumeister <p.baumeister@fz-juelich.de>
2025-06-06 11:03:57 +0200

    refactor test to prepare planewave only when changed

--- include/finite_difference.hxx
+++ include/finite_difference.hxx
@@ -365,51 +365,61 @@ namespace finite_difference {
 #ifdef    GENERAL_CELL
     template <typename real_t>
     inline status_t test_general_cell(int const echo=3) {
+        // ToDo: not covered: more than one shift non-zero at the same time
         status_t stat(0);
         // create a shifted Cartesian unit cell with angles 60 degree
-        double const h[3] = {1/15., std::sqrt(.75)/13., 1/12.}; // grid spacings
-        int const nn[3] = {12, 12, 12}; // FD-order, switch FD lower for the z-direction
+        int const dims[] = {15, 13, 12};
+        double const h[3] = {1./dims[0], std::sqrt(3/4.)/dims[1], std::sqrt(2/3.)/dims[2]}; // grid spacings
+        int const nn[3] = {12, 12, 12}; // FD-order
         stencil_t<real_t> Laplacian(h, nn);
-        int dims[] = {15, 13, 12};
         real_space::grid_t g(dims);
-        g.set_boundary_conditions(Periodic_Boundary, Shifted_Boundary, Shifted_Boundary);
-        if (echo > 1) std::printf("\n# %s start\n", __func__);
-        char const shift_name[3][4] = {"xy", "xz", "yz"};
-        int const shift_dim[] = {dims[0], dims[0], dims[1]};
-        for (int is{0}; is < 3; ++is) { // xy, xz, yz
-        for (int xyz_shift{0}; xyz_shift < shift_dim[is]; ++xyz_shift) {
-        int xyzs[] = {0, 0, 0}; xyzs[is] = xyz_shift;
-        int const xy_shift = xyzs[0], xz_shift = xyzs[1], yz_shift = xyzs[2];
-        double const cell_shape[3][4] = {{h[0]*dims[0],             0,             0, 0},
-                                         {h[0]*xy_shift, h[1]*dims[1],             0, 0},
-                                         {h[0]*xz_shift, h[1]*yz_shift, h[2]*dims[2], 0}}; // lower triangular matrix
-        g.set_cell_shape(cell_shape, echo/2);
         std::vector<std::complex<real_t>> values(g.all()), result(g.all());
         std::complex<real_t> boundary_phase[3][2];
-        auto const arc = constants::pi/180.;
+        g.set_boundary_conditions(Periodic_Boundary, Shifted_Boundary, Shifted_Boundary);
+        if (echo > 1) std::printf("\n# %s start\n", __func__);
+                                    //  000        001   010     011    100    101      110     111
+        char const shift_name[8][8] = {"noshift", "xy", "xz", "xz+yz", "yz", "xy+yz", "xy+xz", "xyxzyz"};
+        double maxdevall{0};
+        for (int is{0}; is < 7; ++is) { // is==7 needs a very long time, test only 0--6 to be faster
+            int const shift_dims[] = {((is >> 0) & 0x1)*(dims[0] - 1),      // max shift along x-direction on crossing a y-boundary
+                                      ((is >> 1) & 0x1)*(dims[0] - 1),      // max shift along x-direction on crossing a z-boundary
+                                      ((is >> 2) & 0x1)*(dims[1] - 1)};     // max shift along y-direction on crossing a z-boundary
+            if (echo > 3) { std::printf("# %s: test xy_shift in [0, %d], xz_shift in [0, %d], yz_shift in [0, %d], name= %s\n",
+                                __func__, shift_dims[0], shift_dims[1], shift_dims[2], shift_name[is]); }
+            double maxdev_is{0};
+            for (int idirection{0}; idirection <= 90; idirection += 15) { // angle
+                // prepare plane wave vector (this could be moved outside the shift loops to save time)
+                double const k = 1.6; // sqRy
+                auto constexpr arc = constants::pi/180.;
+                auto const k_cos = k*std::cos(idirection*arc), k_sin = k*std::sin(idirection*arc);
+                double const kv_dir[] = {k_cos, k_sin, .1, k_cos, k_sin, .1, k_cos, k_sin, .1, k_cos};
+                double const *const kv = kv_dir + is; // only reference *kv as kv[3]
+                // prepare wave values
+                for (int iz{0}; iz < g('z'); ++iz) {
+                    for (int iy{0}; iy < g('y'); ++iy) {
+                        for (int ix{0}; ix < g('x'); ++ix) {
+                            auto const arg = kv[0]*ix*h[0] + kv[1]*iy*h[1] + kv[2]*iz*h[2]; // prepare an arbitrary plane wave
+                            values[(iz*g('y') + iy)*g('x') + ix] = std::complex<real_t>(std::cos(arg), std::sin(arg));
+                        } // ix
+                    } // iy
+                } // iz
+                // prepare matching boundary phases
+                for (int dir{0}; dir < 3; ++dir) {
+                    auto const arg = kv[dir]*g[dir]*h[dir];
+                    boundary_phase[dir][1] = std::complex<real_t>(std::cos(arg), std::sin(arg));
+                    boundary_phase[dir][0] = real_t(1)/boundary_phase[dir][1];
+                    // if (echo > 11) { std::printf("# %s(%d deg) %c-phase= %g %g\n", __func__, idirection, 'x'+dir, boundary_phase[dir][1].real(), boundary_phase[dir][1].imag()); }
+                } // dir
+
         double maxdev{0};
-        for (int idirection{0}; idirection <= 90; idirection += 9) { // angle
-            // prepare plane wave vector
-            double const k = 1.6; // sqRy
-            auto const k_cos = k*std::cos(idirection*arc), k_sin = k*std::sin(idirection*arc);
-            double const kv_dir[] = {k_cos, k_sin, 0, k_cos, k_sin};
-            double const *const kv = kv_dir + is; // only reference *kv as kv[3]
-            // prepare wave values
-            for (int iz{0}; iz < g('z'); ++iz) {
-                for (int iy{0}; iy < g('y'); ++iy) {
-                    for (int ix{0}; ix < g('x'); ++ix) {
-                        auto const arg = kv[0]*ix*h[0] + kv[1]*iy*h[1] + kv[2]*iz*h[2]; // prepare an arbitrary plane wave
-                        values[(iz*g('y') + iy)*g('x') + ix] = std::complex<real_t>(std::cos(arg), std::sin(arg));
-                    } // ix
-                } // iy
-            } // iz
-            // prepare matching boundary phases
-            for (int dir{0}; dir < 3; ++dir) {
-                auto const arg = kv[dir]*g[dir]*h[dir];
-                boundary_phase[dir][1] = std::complex<real_t>(std::cos(arg), std::sin(arg));
-                boundary_phase[dir][0] = real_t(1)/boundary_phase[dir][1];
-                // if (echo > 11) { std::printf("# %s(%d deg) %c-phase= %g %g\n", __func__, idirection, 'x'+dir, boundary_phase[dir][1].real(), boundary_phase[dir][1].imag()); }
-            } // dir
+        for (int xy_shift{0}; xy_shift <= shift_dims[0]; ++xy_shift) {          // shift along x-direction on crossing a y-boundary
+        for (int xz_shift{0}; xz_shift <= shift_dims[1]; ++xz_shift) {          // shift along x-direction on crossing a z-boundary
+        for (int yz_shift{0}; yz_shift <= shift_dims[2]; ++yz_shift) {          // shift along y-direction on crossing a z-boundary
+            if (echo > 13) { std::printf("# %s: test %s-shifts\n", __func__, shift_name[is]); }
+            double const cell_shape[3][4] = {{h[0]*dims[0],             0,             0, 0},
+                                             {h[0]*xy_shift, h[1]*dims[1],             0, 0},  // lower triangular matrix
+                                             {h[0]*xz_shift, h[1]*yz_shift, h[2]*dims[2], 0}};
+            g.set_cell_shape(cell_shape, echo/4);
 
             // apply
             stat += finite_difference::apply(result.data(), values.data(), g, Laplacian, 1, boundary_phase);
@@ -420,17 +430,22 @@ namespace finite_difference {
             for (size_t i{0}; i < g.all(); ++i) {
                 auto const val = values[i], res = result[i], ref = -k2*val; // reference is the analytic solution to the Laplacian operator applied to a plane wave
                 auto const dev_add = std::abs(res - ref);
-                // if (echo > 22 && (i < 30 || i >= 30*25)) { std::printf("# %s(%i, %i) value= %g %g result= %g %g ref= %g %g dev= %g\n",
-                //     __func__, i/g[0], i%g[0], val.real(), val.imag(), res.real(), res.imag(), ref.real(), ref.imag(), dev_add); }
                 dev += dev_add;
             } // i
-            if (echo > 5) std::printf("# %s direction=%4d degrees, dev= %g\n", __func__, idirection, dev/g.all());
+            if (echo > 9) { std::printf("# %s direction=%4d degrees xy= %d/%d, xz= %d/%d, yz= %d/%d, dev= %g\n", __func__,
+                                  idirection,  xy_shift, dims[0],  xz_shift, dims[0],  yz_shift, dims[1],  dev/g.all()); }
             maxdev = std::max(maxdev, std::abs(dev/g.all()));
-        } // idirection
-        if (echo > 1) std::printf("# %s largest deviation for %s-shift= %d of %d is %.1e\n", __func__, shift_name[is], xyz_shift, shift_dim[is], maxdev);
+        }}} // *_shift
+
         stat += (maxdev > 1e-12);
-        } // xy_shift
+        if (echo > 4) std::printf("# %s direction=%4d degrees, dev= %g\n", __func__, idirection, maxdev);
+        maxdev_is = std::max(maxdev_is, maxdev);
+    } // idirection
+
+        if (echo > 0) { std::printf("# %s largest deviation tests \'%s\' is %.1e\n", __func__, shift_name[is], maxdev_is); }
+        maxdevall = std::max(maxdevall, maxdev_is);
         } // is
+        if (echo > 0) { std::printf("# %s largest deviation of all tests is %.1e\n", __func__, maxdevall); }
         return stat;
     } // test_general_cell
 #endif // GENERAL_CELL
